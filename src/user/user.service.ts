d239dbfefab32959398071b7c163285b53de7d8b
KeniBeck <deyler_mena69222@elpoli.edu.co>
2024-08-23 01:14:19 -0500

    creacion de recurso en modelo User metodos POST & GET

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private prismaService: PrismaService) { }
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.prismaService.user.create({
+      data: {
+        id: createUserDto.id,
+        email: createUserDto.email,
+        name: createUserDto.name,
+        phone: createUserDto.phone,
+        password_hash: createUserDto.password_hash,
+        img_perfil_url: createUserDto.img_perfil_url,
+
+      }
+
+    });
+
+    return newUser;
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
